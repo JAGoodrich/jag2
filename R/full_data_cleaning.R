bbb5d858daddff3e4b72b351533f0d5ac10cccba
Jesse Goodrich <jagoodri@usc.edu>
2020-03-11 13:07:42 -0700

    First Commit

---
+++ R/full_data_cleaning.R
@@ -0,0 +1,27 @@
+#' Clean LC-MS Data
+#' 
+#' @import tidyverse
+#' 
+#' @export
+full_data_cleaning <- function(data, pct_metab_na_allowed, pct_obs_na_allowed){
+  metabolites <- data %>%
+    select((match("lg_pfda",names(data)) + 1):ncol(data))
+  
+  data <- data %>% 
+    select("id":"lg_pfda")
+  
+  # Specify Fraction Metabolite Missing Allowed
+  mv <- pct_metab_na_allowed
+  # Drop metabolites with more missing than allowed
+  metabolites <- metabolites[, which(colMeans(is.na(metabolites)) < mv)]
+  
+  data <- cbind(data, metabolites)
+  
+  # Specify Fraction Missing Metabolies allowed for each observation
+  mv_id <- pct_obs_na_allowed
+  
+  data <- data %>% 
+    filter(rowMeans(is.na(metabolites)) < mv_id)
+  
+  return(data)
+}
---
+++ R/mwas.R
@@ -0,0 +1,29 @@
+#' MWAS Function
+#'
+#' @import tidyverse
+#' 
+#' @export
+mwas <- function(data, name_of_exposure){
+  MWAS_output_lm <- data %>%
+    dplyr::select( (match("lg_pfda",names(data)) + 1):ncol(data)) %>%  # exclude outcome, leave only predictors
+    scale(.) %>% as_tibble() %>%
+    map(~lm(data[[name_of_exposure]] ~ .x + data$og_age + data$sex + data$og_bmi, data = data)) %>%
+    map(summary)
+  
+  MWAS_output <- tibble(name = names(MWAS_output_lm),
+                        exposure = name_of_exposure,
+                        beta = map_dbl(MWAS_output_lm, function(x){coefficients(x)[2,1]}),
+                        se = map_dbl(MWAS_output_lm, function(x){coefficients(x)[2,2]}),
+                        pvalue = map_dbl(MWAS_output_lm, function(x){coefficients(x)[2,4]}),
+                        `t-score` = map_dbl(MWAS_output_lm, function(x){coefficients(x)[2,3]})) 
+  
+  # Calculate FDR Adjustments
+  MWAS_output <- MWAS_output %>% 
+    mutate(qvalue = p.adjust(pvalue, method = "fdr"), # Calculate FDR Adjustment  
+           Significance = ifelse(pvalue < 0.05, "P<0.05", "Not Sig"), 
+           SignificanceFDR = ifelse(qvalue < 0.05, "P<0.05", "Not Sig"), 
+           names = if_else(pvalue < 0.05, name, ""), 
+           namesFDR = if_else(qvalue < 0.05, name, ""), ) 
+  return(MWAS_output)
+}
+
