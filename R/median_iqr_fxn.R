49f9c0d3e08af41826084d138be2603f3e151417
JAGoodrich <jagoodri@usc.edu>
2022-03-27 16:06:24 -0700

    add median_iqr_fxn

---
+++ R/median_iqr_fxn.R
@@ -0,0 +1,17 @@
+#' Get formatted character vector of the Mean (IQR). 
+#' 
+#' @import tidyverse
+#' 
+#' @export
+#' 
+#' @param x Numeric Vector
+#' @param n.digits number of digits after the decimal point. Default is 1.
+median_iqr_fxn <- function(x, n.digits = 1) {
+  out <- paste0(round(median(x, na.rm = T), n.digits) %>% 
+                 formatC(. , format="f", digits=n.digits),
+               " (",
+               round(IQR(x, na.rm = T), n.digits) %>% 
+                 formatC(. , format="f", digits=n.digits), 
+               ")") 
+  return(out)
+}
